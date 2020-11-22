6799847bade89b6ad1fca03095ca23c04cf4a481
Balazs <balazs@otakomaiya.com>
2020-11-22 16:17:29 +0000

    Replaced docs links

--- resources/js/Pages/Lists/Show.vue
+++ resources/js/Pages/Lists/Show.vue
@@ -76,7 +76,7 @@
             <div class="mt-5">
               <span class="inline-flex rounded-md shadow-sm">
                 <a
-                  href="https://await.readme.io/"
+                  href="https://docs.getawait.com"
                   type="button"
                   target="_blank"
                   class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
