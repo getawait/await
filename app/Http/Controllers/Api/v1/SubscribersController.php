7484dcddc77bddeecc64ffc06362bb6de4889f71
Balazs <balazs@otakomaiya.com>
2020-11-27 10:24:50 +0000

    Fix incorrect position and cross-waitlist referrers

--- app/Http/Controllers/Api/v1/SubscribersController.php
+++ app/Http/Controllers/Api/v1/SubscribersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
 use App\Models\Subscriber;
+use App\Rules\ReferrerInWaitlist;
 use App\Rules\UniqueSubscriberEmailForWaitlist;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,7 +20,11 @@ class SubscribersController extends Controller
                 'email',
                 new UniqueSubscriberEmailForWaitlist($request->get('waitlist')),
             ],
-            'referrer' => 'string|exists:subscribers,id',
+            'referrer' => [
+                'string',
+                'exists:subscribers,id',
+                new ReferrerInWaitlist($request->get('waitlist')),
+            ],
         ]);
 
         $subscriber = new Subscriber();
@@ -34,6 +39,7 @@ class SubscribersController extends Controller
         $subscriber->save();
 
         return response()->json([
+            'id' => $subscriber->id,
             'position' => $subscriber->getPosition(),
         ], Response::HTTP_CREATED);
     }
@@ -42,7 +48,7 @@ class SubscribersController extends Controller
     {
         return response()->json([
             'position' => $subscriber->getPosition(),
-            'total' => Subscriber::count(),
+            'total' => Subscriber::where('waitlist_id', $subscriber->waitlist_id)->count(),
         ]);
     }
 }
